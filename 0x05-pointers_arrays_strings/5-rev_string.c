cfec6d9c6445ec0b293f5146479aba6c792a1293
Ajiwojuolorun Joshua <ajiwojuolorunjoshua@gmail.com>
2023-10-16 23:44:16 +0100

    Add files via upload

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * rev_string - Reverses a string
+ * @s: Pointer to the string */
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	char *start = s;
+	char *end = s + len - 1;
+	char temp;
+
+	while (start < end)
+	{
+		temp = *start;
+		*start = *end;
+		*end = temp;
+
+		start++;
+		end--;
+	}
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+# 0x05-pointers_arrays_strings
+
+## 0x05. Pointers, Arrays && Strings
+
+This Repository contains the tasks for 0x05-pointers_arrays_strings.
+
