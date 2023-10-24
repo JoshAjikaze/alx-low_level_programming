cdacc0b3c93ee5fdf95a5bd07fa4ca9e1c4aa88a
Ajiwojuolorun Joshua <ajiwojuolorunjoshua@gmail.com>
2023-10-24 10:46:54 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,47 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
+ * _memset -fills memory with a constant byte
+ * @s: area to fill with bytes
+ * @b: constant bytes
+ * @n: number of bytes
+ * Return: pointer to s.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+char *_memset(char *s, char b, unsigned int n)
 {
-  unsigned int i;
+	unsigned int i;
+	for(1 = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
 
-  i = 0;
-  while (i < size)
-    {
-      if (i % 10)
-        {
-	  printf(" ");
-        }
-      if (!(i % 10) && i)
-        {
-	  printf("\n");
-        }
-      printf("0x%02x", buffer[i]);
-      i++;
-    }
-  printf("\n");
-}
-
-/**
- * main - check the code 
- *
- * Return: Always 0.
- */
-int main(void)
-{
-  char buffer[98] = {0};
-  char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-
-  simple_print_buffer(buffer, 98);
-  _memcpy(buffer + 50, buffer2, 10);
-  printf("-------------------------------------------------\n");
-  simple_print_buffer(buffer, 98);    
-  return (0);
+	return(s);
 }
