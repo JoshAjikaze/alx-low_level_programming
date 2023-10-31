1cefcbd1c3bdef9381fdda635acf53bea194cf24
Ajiwojuolorun Joshua <ajiwojuolorunjoshua@gmail.com>
2023-10-31 20:52:42 +0100

    commit 2

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - creates 2 dimensional array of integers
+ * @width: width of the array
+ * @height: height of the array
+ *
+ * Return: pointer to the array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (i = i - 1; i >= 0; i--)
+			{
+				free(grid[i]);
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all arguments
+ * @ac: number of arguments
+ * @av: argument values
+ *
+ * Return: pointer to new string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *string;
+	int length;
+	int i, j, k = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			length++;
+	}
+	string = malloc(sizeof(char) * (length + ac + 1));
+	if (string == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			string[k] = av[i][j];
+			k++;
+		}
+		string[k] = '\n';
+		k++;
+	}
+	string[k] = '\0';
+	return (string);
+}
