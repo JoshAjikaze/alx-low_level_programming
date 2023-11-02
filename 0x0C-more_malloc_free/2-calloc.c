55e65dcf61a694a9b31e528fc4c75ecf72ec2ba1
Ajiwojuolorun Joshua <ajiwojuolorunjoshua@gmail.com>
2023-11-02 22:24:53 +0100

    commit 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: type of element
+ * @size: amount of elements
+ *
+ * Return: pointer to allocated memory, NULL otherwise
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *pointer;
+	unsigned int i;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	pointer = malloc(nmemb * size);
+	if (pointer == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+	{
+		*((char *)pointer + i) = 0;
+	}
+	return (pointer);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers
+ * @min: start of the array
+ * @max: end of the array
+ *
+ * Return: pointer to newly created array
+ */
+
+int *array_range(int min, int max)
+{
+	int length;
+	int *int_array;
+	int i;
+	int k = 0;
+
+	if (min > max)
+		return (NULL);
+
+	length = (max - min) + 1;
+	int_array = malloc(sizeof(int) * length);
+	if (int_array == NULL)
+		return (NULL);
+
+	for (i = min; i <= max; i++)
+	{
+		int_array[k] = i;
+		k++;
+	}
+	return (int_array);
+}
