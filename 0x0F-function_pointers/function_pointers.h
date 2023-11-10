1265d445c07a701cc7949ed48568f1e14969f44a
Ajiwojuolorun Joshua Ayomide <ajiwojuolorunjoshua@gmail.com>
2023-11-10 13:55:20 -0800

    yet another commit for task 1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,22 +2,12 @@
 #define FUNCTION_POINTERS_H
 #include <stdio.h>
 
-/**
- * struct data_type - struct for character type
- *
- * @type: the type selector
- * @f: The function associated
- */
-typedef struct data_type
-{
-	char *data_type;
-	void (*f)(char *string);
-} data_type_t;
+#include<stddef.h>
+#include<stdlib.h>
 
 
-
-int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 #endif
